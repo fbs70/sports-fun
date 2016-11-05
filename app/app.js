eb520a553bed05305b4266d3e666025574ebd0ae
Blair Shotlow <blair.shotlow@gmail.com>
2016-11-04 20:00:47 -0500

    minor updates

--- app/app.js
+++ app/app.js
@@ -1,35 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('sportsApp', ['ui.router']);
-
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'partials/home.html',
-      controller: 'MainCtrl'
-    })
-    .state('nba', {
-      url: '/nba',
-      templateUrl: 'partials/nba.html',
-      controller: 'NbaCtrl'
-    })
-    .state('nfl', {
-      url: '/nfl',
-      templateUrl: 'partials/nfl.html',
-      controller: 'NflCtrl'
-    })
-
-  $urlRouterProvider.otherwise('home');
-}]);
+var app = angular.module('sportsApp', ['ui.router', 'ngAnimate' ,'ngTouch']);
 
 app.controller('MainCtrl', ['$scope', function($scope){
-  $scope.isNavCollapsed = true;
 }]);
 
 app.controller('NbaCtrl', ['$scope', '$http', function($scope, $http){
   //$scope.isNavCollapsed = true;
+  $scope.nbaTeam = [ ];
 
   $http.get('data/nba-team-info.json')
     .success(function(response){
@@ -40,9 +19,9 @@ app.controller('NbaCtrl', ['$scope', '$http', function($scope, $http){
 
 app.controller('NflCtrl', ['$scope', '$http', function($scope, $http){
   //$scope.isNavCollapsed = true;
+  $scope.nflTeam = [ ];
 
-  $http.get('data/nfl-team-info.json')
-    .success(function(response){
-      $scope.nflTeam = response.nfl;
+  $http.get('data/nfl-team-info.json').success(function(data){
+      $scope.nflTeam = data.nfl;
     });
 }]);
